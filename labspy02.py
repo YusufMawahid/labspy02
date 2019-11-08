cdaf68cb000953cd0b312eda48588483d68217a5
YusufMawahid <wahidajjah52@gmail.com>
2019-11-09 01:07:42 +0700

    first commit

---
+++ labspy02.py
@@ -0,0 +1,11 @@
+
+a = int(input("bilangan 1 ="))
+b = int(input("bilangan 2 ="))
+c = int(input("bilangan 3 ="))
+
+if a>b and a>c:
+	print(a, "terbesar")
+elif b>a and b>c:
+	print(b, "terbesar")
+else:
+	print(c, "terbesar")
